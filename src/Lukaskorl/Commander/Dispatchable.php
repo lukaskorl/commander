383ea50ac25736e79578b0a5fb950b409d885ea4
Lukas Korl <hello@lukaskorl.com>
2014-07-28 19:43:36 +0200

    Inlcude a convenience trait for dispatching events in Laravel

---
+++ src/Lukaskorl/Commander/Dispatchable.php
@@ -0,0 +1,29 @@
+<?php namespace Lukaskorl\Commander;
+
+use Lukaskorl\Commander\Dispatcher\EventDispatcher;
+use App;
+
+trait Dispatchable {
+
+    /**
+     * Dispatch and flush all events for an event raising object
+     *
+     * @param Eventable $raiser
+     * @return mixed
+     */
+    public function dispatchAndFlushEventsFor(Eventable $raiser)
+    {
+        return $this->getEventDispatcher()->dispatch($raiser->flushEvents());
+    }
+
+    /**
+     * Instantiate the event dispatcher
+     *
+     * @return EventDispatcher
+     */
+    public function getEventDispatcher()
+    {
+        return App::make('Lukaskorl\Commander\Dispatcher\EventDispatcher');
+    }
+
+} 
